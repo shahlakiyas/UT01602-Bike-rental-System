88545532d924f62f1d38993b78403b2ec01028d5
UT01317 <UT01317@UNICOMTIC24>
2024-10-03 16:30:49 +0530

    Confirm Rent in modal Show in backend data retrive

--- Bike_Rental_website-main/js/server.js
+++ Bike_Rental_website-main/js/server.js
@@ -21,9 +21,11 @@ async function fetchProducts() {
                     </div>
                     <h3 class="bike-price">₹${bike.ratePerHour}<span>/hour</span></h3>
                 </div>
-                <a href="#" class="book-btn" onclick="fetchbikeById(${bike.bikeId})" data-bs-toggle="modal" data-bs-target="#exampleModal">Book Bike</a>
+                <a href="#" class="book-btn" onclick="fetchbikeById(${bike.bikeId})">Book Bike</a>
                 <span class="tag">${bike.tag}</span>
             </div>
+
+        
 `;
     const bikeContent = document.querySelector(".bikes-content");
     // create bike box and show in bikecontent div
@@ -34,21 +36,67 @@ async function fetchProducts() {
 }
 
 
-async function fetchbikeById(id) {
+// async function fetchbikeById(id) {
     
-    let confirmRent = document.getElementById('confirmRent')
+//     let confirmRent = document.getElementById('confirmRent')
+//     confirmRent.style.display = "block"
 
+//     const response = await fetch(`${apiBaseUrl}${id}`);
+//     const bike = await response.json();
+//     console.log(bike);
+//     printConfirmRent(bike,confirmRent)
+// }
 
-    const response = await fetch(`${apiBaseUrl}${id}`);
+// function printConfirmRent(bikeObj,rentalDiv){
+//     //  rentalDiv.innerHTML = "";
+//      console.log(bikeObj);     
+
+//      rentalDiv.innerHTML =+ `
+//      <div class="bike-box">
+//      <p style='display:none';>${bikeObj.bikeId}</p>
+//      <img src="${bikeObj.bikeImages[0].imagePath}" alt="" class="box-img" />
+//      <div class="title-price">
+//          <div class="title-data">
+//              <h2>${bikeObj.modal}</h2>
+//              <p>${bikeObj.type}</p>
+//          </div>
+//          <h3 class="bike-price">₹${bikeObj.ratePerHour}<span>/hour</span></h3>
+//      </div>
+//      <a href="#" class="book-btn" onclick="fetchbikeById(${bikeObj.bikeId})">Book Bike</a>
+//      <span class="tag">${bikeObj.tag}</span>
+//  </div>
+//      `
+// }
+
+async function fetchbikeById(id) {
+    let confirmRent = document.getElementById('confirmRent');
+    confirmRent.style.display = "block";
+
+    const response = await fetch(`${apiBaseUrl}/${id}`);
     const bike = await response.json();
     console.log(bike);
-    printConfirmRent(bike,confirmRent)
+    printConfirmRent(bikeObj, rentalDiv);
 }
 
-function printConfirmRent(bikeObj,rentalDiv){
-     rentalDiv.innerHTML = "";
-     console.log(bikeObj);     
+function printConfirmRent(bikeObj, rentalDiv) {
+    console.log(bikeObj);
+
+    rentalDiv.innerHTML = `
+    <div class="bike-box">
+        <p style='display:none;'>${bikeObj.bikeId}</p>
+        <img src="${bikeObj.bikeImages[0].imagePath}" alt="" class="box-img" />
+        <div class="title-price">
+            <div class="title-data">
+                <h2>${bikeObj.modal}</h2>
+                <p>${bikeObj.type}</p>
+            </div>
+            <h3 class="bike-price">₹${bikeObj.ratePerHour}<span>/hour</span></h3>
+        </div>
+    </div>
+    `;
 }
+
+
 
 
 
