280a573f7d7bb5a4c561c05431f3d7af1c5f727b
UT01317 <UT01317@UNICOMTIC24>
2024-10-04 13:37:23 +0530

    Rental confirm model retrive data sample in Products Array. I will change fetch Api

--- backend/BikeRentalApplication/BikeRentalApplication/obj/Debug/net8.0/BikeRentalApplication.AssemblyInfo.cs
+++ backend/BikeRentalApplication/BikeRentalApplication/obj/Debug/net8.0/BikeRentalApplication.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BikeRentalApplication")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8f2a93961f90ab549e578e74eac2b860f8721337")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2fd48524553cc08e117de4a84259308d80d14d1f")]
 [assembly: System.Reflection.AssemblyProductAttribute("BikeRentalApplication")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BikeRentalApplication")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
