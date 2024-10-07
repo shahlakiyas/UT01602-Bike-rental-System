1795db7459e8f1bdb56214b7052ef33896ef06ef
Paramananthan T. <UT01373@unicomtic.com>
2024-10-07 17:26:52 +0530

    DBset completed && Rental Request successful

---
+++ backend/BikeRentalApplication/BikeRentalApplication/Controllers/UserController.cs
@@ -0,0 +1,28 @@
+﻿using BikeRentalApplication.Entities;
+using BikeRentalApplication.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BikeRentalApplication.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly UserRepository _userRepository;
+
+
+        public UserController(UserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        // Create User
+        [HttpPost]
+        public async Task<IActionResult> CreateUser(User user)
+        {
+            var NICNo = await _userRepository.CreateUserAsync(user);
+            return Ok(NICNo);
+        }
+    }
+}
---
+++ backend/BikeRentalApplication/BikeRentalApplication/Entities/User.cs
@@ -0,0 +1,16 @@
+﻿namespace BikeRentalApplication.Entities
+{
+    public class User
+    {
+      public string  NICNumber { get; set; }
+        public string FirstName { get; set; }   
+        public string LastName { get; set; }
+        public string Email { get; set; }   
+        public int ContactNo { get; set; }
+        public string Address { get; set; }
+        public string userName { get; set; }
+        public string Password { get; set; }
+        public string Role{ get; set; }
+        public DateTime AccountCreated { get; set; }
+    }
+}
